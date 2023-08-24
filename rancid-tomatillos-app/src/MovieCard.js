45f0864f5cabbaa011e830a2a6e2b10d98a7ff3a
Banjamin Hatch <banjaminhatch@gmail.com>
2023-08-24 15:35:55 -0600

    Shorten the ratings to 1 decimal

--- rancid-tomatillos-app/src/MovieCard.js
+++ rancid-tomatillos-app/src/MovieCard.js
@@ -1,11 +1,12 @@
 import './MovieCard.css'
 
 function MovieCard({id , title, poster_path, backdrop_path, average_rating, release_date}) {
+    let shortenedRating = average_rating.toFixed(1)
     return (
         <div className="movie-card">
             <h3>{title}</h3>
             <img src={poster_path}></img>
-            <p>{average_rating}</p>
+            <p>{shortenedRating}</p>
         </div>
     )
 }
