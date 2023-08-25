a8feb458c6c15e251ab70816d2b3ce1114a4c933
Benjamin holden <benjamin.f.holden@outlook.com>
2023-08-24 18:01:05 -0600

    Add Selected Movie page and functionality

--- rancid-tomatillos-app/src/MovieCard.js
+++ rancid-tomatillos-app/src/MovieCard.js
@@ -1,9 +1,9 @@
 import './MovieCard.css'
 
-function MovieCard({id , title, poster_path, backdrop_path, average_rating, release_date}) {
+function MovieCard({id , title, poster_path, backdrop_path, average_rating, release_date, displayMovie}) {
     let shortenedRating = average_rating.toFixed(1)
     return (
-        <div className="movie-card">
+        <div className="movie-card" onClick={() => displayMovie(id)}>
             <h3>{title}</h3>
             <img src={poster_path}></img>
             <p>{shortenedRating}</p>
