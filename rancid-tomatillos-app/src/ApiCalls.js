8f907bafdf0a563112d9841de08338bd405a283e
Banjamin Hatch <banjaminhatch@gmail.com>
2023-08-26 16:07:33 -0600

    Add a few CSS changes

--- rancid-tomatillos-app/src/ApiCalls.js
+++ rancid-tomatillos-app/src/ApiCalls.js
@@ -3,15 +3,18 @@
 export function fetchMovies(){
     return fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
     .then(response => response.json())
+    .catch(error => alert(error.message))
 }
 
 export function fetchSingleMovie(id){
     return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
     .then(response => response.json())
+    .catch(error => alert(error.message))
 }
 
 export function fetchMovieVideo(id){
     return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}/videos`)
     .then(response => response.json())
+    .catch(error => alert(error.message))
 }
 
