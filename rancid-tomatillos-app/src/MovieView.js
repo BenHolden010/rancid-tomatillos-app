f1544e997e33787a5272bbff09cfd3a4f7f075d6
Banjamin Hatch <banjaminhatch@gmail.com>
2023-08-27 11:47:58 -0600

    Add overlay and separated the movie trailer

--- rancid-tomatillos-app/src/MovieView.js
+++ rancid-tomatillos-app/src/MovieView.js
@@ -4,27 +4,33 @@ function MovieView({backdrop_path,id,title,release_date,average_rating,showMovie
 let revenueEdit = revenue.toLocaleString('en-US', { style: 'currency', currency: 'USD'})
 let budgetEdit = budget.toLocaleString('en-US', { style: 'currency', currency: 'USD'})
 return (
-  <div className="selectedMovie" style={{backgroundImage: `url(${backdrop_path})`}}>
+  <div className='selected-movie-container'>
     <button className='back-button' onClick={() => {showMovies()}}>Back</button>
-    {/* <div className='tagline-container'>
-      <p className="tagline">{tagline}</p>
-    </div> */}
     <div className='video-container'>
-      <iframe allowFullScreen src={`https://www.youtube.com/embed/${selectedVideos}?autoplay=1`} allow='autoplay' frameBorder='0' >
-      </iframe>
-    </div>
-    <div className='movie-info'>
-      <div className='top-info'>
-        <h3>{title}</h3>
-        <p>{genres.join(', ')}</p>
-        <p>{runtime} minutes</p>
-      </div>
-      <div className='bottom-info'>
-        <p>{overview}</p>
-        {/* <p>Movie Rating: {average_rating.toFixed(1)}/10</p>
-        <p>Release Date: {release_date}</p>
-        {revenue ? <p>Budget: {budgetEdit}</p> : <p>Budget: Not available</p>}
-        {budget  ? <p>Revenue: {revenueEdit}</p> : <p>Revenue: Not available</p>} */}
+        <iframe allowFullScreen src={`https://www.youtube.com/embed/${selectedVideos}?autoplay=1`} allow='autoplay' frameBorder='0' >
+        </iframe>
+    </div> 
+    <div className="selectedMovie" style={{backgroundImage: `url(${backdrop_path})`}}>
+      {/* <div className='tagline-container'>
+        <p className="tagline">{tagline}</p>
+      </div> */}
+      {/* <div className='video-container'>
+        <iframe allowFullScreen src={`https://www.youtube.com/embed/${selectedVideos}?autoplay=1`} allow='autoplay' frameBorder='0' >
+        </iframe>
+      </div> */}
+      <div className='movie-info'>
+        <div className='top-info'>
+          <h3>{title}</h3>
+          <p>{genres.join(', ')}</p>
+          <p>{runtime} minutes</p>
+        </div>
+        <div className='bottom-info'>
+          <p>{overview}</p>
+          {/* <p>Movie Rating: {average_rating.toFixed(1)}/10</p>
+          <p>Release Date: {release_date}</p>
+          {revenue ? <p>Budget: {budgetEdit}</p> : <p>Budget: Not available</p>}
+          {budget  ? <p>Revenue: {revenueEdit}</p> : <p>Revenue: Not available</p>} */}
+        </div>
       </div>
     </div>
   </div>
